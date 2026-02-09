c6cfc9bfe23015a1e1f2f916b68fe4a7750144d9
Nissyaniss <nissya@proton.me>
2026-02-09 18:26:29 +0100

    make sddm not compatible with laptop/pc system

--- config/nix/system/aesthetics/sddm.nix
+++ config/nix/system/aesthetics/sddm.nix
@@ -1,27 +1,36 @@
 { pkgs
 , lib
+, machine-name
 , ...
-}: {
+}:
+let
+  isWayland = if machine-name == "pc" then false else true;
+  extraSddmSettingsPc =
+    if machine-name == "pc" then {
+      setupScript = ''
+        export QT_IM_MODULE=""
+        export QT_VIRTUAL_KEYBOARD_STYLE=""
+        ${pkgs.xorg.xrandr}/bin/xrandr --output DP-2 --primary --mode 2560x1440 --rate 144 --rotate normal --output HDMI-1 --off
+      '';
+
+      settings = {
+        General = { InputMethod = ""; };
+      };
+    } else { };
+  extraSddmModificationsPc =
+    if machine-name == "pc" then {
+      systemd.services.display-manager.environment = {
+        QT_IM_MODULE = lib.mkForce "none";
+        QT_VIRTUALKEYBOARD_DESKTOP_DISABLE = lib.mkForce "1";
+      };
+      services.xserver.enable = true;
+    } else { };
+in
+{
   services.displayManager.sddm = {
     enable = true;
-    wayland = {
-      enable = false;
-    };
+    wayland.enable = isWayland;
     theme = "sddm-black_hole-theme";
     extraPackages = [ pkgs.sddm-astronaut ];
-    setupScript = ''
-      export QT_IM_MODULE=""
-      export QT_VIRTUAL_KEYBOARD_STYLE=""
-      ${pkgs.xorg.xrandr}/bin/xrandr --output DP-2 --primary --mode 2560x1440 --rate 144 --rotate normal --output HDMI-1 --off
-    '';
-
-    settings = {
-      General = { InputMethod = ""; };
-    };
-  };
-  systemd.services.display-manager.environment = {
-    QT_IM_MODULE = lib.mkForce "none";
-    QT_VIRTUALKEYBOARD_DESKTOP_DISABLE = lib.mkForce "1";
-  };
-  services.xserver.enable = true;
-}
+  } // extraSddmSettingsPc;
+} // extraSddmModificationsPc
