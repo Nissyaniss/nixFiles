54a538c74e062a6e7201dd29bc5e8034ab4a6490
Nissyaniss <nissya@proton.me>
2026-02-10 22:15:43 +0100

    finished yay

---
+++ config/home-manager/shell/eww/arrows.nix
@@ -0,0 +1,26 @@
+{
+  leftEndingArrow = ''
+    (label
+      :text ""
+      :class "left-ending-arrow"
+    )
+  '';
+  leftStartingArrow = ''
+    (label
+      :text ""
+      :class "left-starting-arrow"
+    )
+  '';
+  rightEndingArrow = ''
+    (label
+      :text ""
+      :class "right-ending-arrow"
+    )
+  '';
+  rightStartingArrow = ''
+    (label
+      :text ""
+      :class "right-starting-arrow"
+    )
+  '';
+}
--- modules/home-manager/eww.nix
+++ modules/home-manager/eww.nix
@@ -163,365 +163,7 @@ let
     }
   );
 
-  defWidgetOptions = types.submodule (
-    { name, ... }:
-    {
-      options = {
-        name = mkOption {
-          type = types.str;
-          default = name;
-          description = ''
-            Name of the widget.
-          '';
-        };
-        children = {
-          type = children;
-          description = ''
-            Children of the widget.
-          '';
-        };
-      };
-    }
-  );
 
-  widgetDefaultAttributes =
-    {
-      class = mkOption {
-        type = types.str;
-        example = "good-lookin-class";
-        description = ''
-          Css class name.
-        '';
-      };
-      valign = mkOption {
-        type = types.str;
-        example = "fill";
-        description = ''
-          How to align this vertically. possible values: "fill", "baseline", "center", "start", "end".
-        '';
-      };
-      halign = mkOption {
-        type = types.str;
-        example = "fill";
-        description = ''
-          How to align this horizontally. possible values: "fill", "baseline", "center", "start", "end"
-        '';
-      };
-      vexpand = mkOption {
-        type = types.bool;
-        default = false;
-        example = "fill";
-        description = ''
-          Should this container expand vertically.
-        '';
-      };
-      hexpand = mkOption {
-        type = types.bool;
-        default = false;
-        description = ''
-          Should this widget expand horizontally.
-        '';
-      };
-      width = mkOption {
-        type = types.int;
-        description = ''
-          Width of this element. note that this can not restrict the size if the contents stretch it.
-        '';
-      };
-      height = mkOption {
-        type = types.int;
-        description = ''
-          Height of this element. note that this can not restrict the size if the contents stretch it.
-        '';
-      };
-      active = mkOption {
-        type = types.bool;
-        default = true;
-        description = ''
-          If this widget can be interacted with.
-        '';
-      };
-      tooltip = mkOption {
-        type = types.str;
-        default = "";
-        description = ''
-          Tooltip text (on hover)
-        '';
-      };
-      visible = mkOption {
-        type = types.bool;
-        default = true;
-        description = ''
-          Visibility of the widget
-        '';
-      };
-      style = mkOption {
-        type = types.lines;
-        default = "";
-        description = ''
-          Inline scss style applied to the widget
-        '';
-      };
-      css = mkOption {
-        type = types.lines;
-        default = "";
-        description = ''
-          Scss code applied to the widget.
-        '';
-      };
-    };
-
-  children = types.oneOf [
-    centerBox
-    box
-    button
-    label
-    systray
-    children
-  ];
-
-  centerBox = types.submodule (
-    { ... }:
-    {
-      options = {
-        orientation = mkOption {
-          type = types.str;
-          example = "horizontal";
-          description = ''
-            Orientation of the centerbox.
-          '';
-        };
-        children = {
-          type = types.attrsOf children;
-          example = "coming soon";
-          description = ''
-            List of childrens.
-          '';
-        };
-      } // widgetDefaultAttributes;
-    }
-  );
-
-  box = types.submodule (
-    { ... }:
-    {
-      options = {
-        space-evenly = mkOption {
-          type = types.bool;
-          description = ''
-            Whether 
-          '';
-        };
-        orientation = mkOption {
-          type = types.str;
-          example = "horizontal";
-          description = ''
-            Orientation of the centerbox.
-          '';
-        };
-        spacing = {
-          type = types.int;
-          example = "coming soon";
-          description = ''
-            Spacing between elements.
-          '';
-        };
-        children = {
-          type = types.attrsOf children;
-          example = "coming soon";
-          description = ''
-            List of childrens.
-          '';
-        };
-      } // widgetDefaultAttributes;
-    }
-  );
-
-  button = types.submodule (
-    { ... }:
-    {
-      options = {
-        timeout = {
-          type = types.str;
-          default = "200ms";
-          description = ''
-            Duration timeout of the command.
-          '';
-        };
-        onclick = {
-          type = types.str;
-          description = ''
-            Command to run when the button is activated either by leftclicking or keyboard.
-          '';
-        };
-        onmiddleclick = {
-          type = types.str;
-          description = ''
-            Command to run when the button is middleclicked.
-          '';
-        };
-        onrightclick = {
-          type = types.str;
-          description = ''
-            Command to run when the button is rightclicked.
-          '';
-        };
-      } // widgetDefaultAttributes;
-    }
-  );
-
-  label = types.submodule (
-    { ... }: {
-      options = {
-        text = {
-          type = types.str;
-          description = ''
-            The text to display.
-          '';
-        };
-        truncate = {
-          type = types.nullOr types.bool;
-          default = false;
-          description = ''
-            Whether to truncate text (or pango markup). If show-truncated is false, or if limit-width has a value, this property has no effect and truncation is enabled.
-          '';
-        };
-        limit-width = {
-          type = types.nullOr types.int;
-          default = null;
-          description = ''
-            Maximum count of characters to display.
-          '';
-        };
-        truncate-left = {
-          type = types.bool;
-          default = false;
-          description = ''
-            Whether to truncate on the left side.
-          '';
-        };
-        show-truncated = {
-          type = types.bool;
-          default = false;
-          description = ''
-            Show whether the text was truncated. Disabling it will also disable dynamic truncation (the labels won't be truncated more than limit-width, even if there is not enough space for them), and will completly disable truncation on pango markup.
-          '';
-        };
-        unindent = {
-          type = types.bool;
-          default = true;
-          description = ''
-            Whether to remove leading spaces.
-          '';
-        };
-        markup = {
-          type = types.nullOr types.str;
-          default = null;
-          description = ''
-            Pango markup to display.
-          '';
-        };
-        wrap = {
-          type = types.bool;
-          default = false;
-          description = ''
-            Wrap the text. This mainly makes sense if you set the width of this widget.
-          '';
-        };
-        angle = {
-          type = types.ints.between 0 360;
-          default = 0;
-          description = ''
-            The angle of rotation for the label (between 0 - 360).
-          '';
-        };
-        gravity = {
-          type = types.nullOr types.str;
-          default = null;
-          description = ''
-            The gravity of the string (south, east, west, north, auto). Text will want to face the direction of gravity.
-          '';
-        };
-        xalign = {
-          type = types.nullOr types.ints.between 0 1;
-          default = null;
-          description = ''
-            The alignment of the label text on the x axis (between 0 - 1, 0 -> bottom, 0.5 -> center, 1 -> top).
-          '';
-        };
-        yalign = {
-          type = types.nullOr types.ints.between 0 1;
-          default = null;
-          description = ''
-            The alignment of the label text on the y axis (between 0 - 1, 0 -> bottom, 0.5 -> center, 1 -> top).
-          '';
-        };
-        justify = {
-          type = types.nullOr types.str;
-          default = null;
-          description = ''
-            The justification of the label text (left, right, center, fill).
-          '';
-        };
-        wrap-mode = {
-          type = types.nullOr types.str;
-          default = null;
-          description = ''
-            How text is wrapped. possible options: "word", "char", "wordchar".
-          '';
-        };
-        lines = {
-          type = types.int;
-          default = -1;
-          description = ''
-            Maximum number of lines to display (only works when limit-width has a value). A value of -1 (default) disables the limit.
-          '';
-        };
-      } // widgetDefaultAttributes;
-    }
-  );
-
-  systray = types.submodule (
-    { ... }: {
-      options = {
-        spacing = {
-          type = types.nullOr types.int;
-          default = null;
-          description = ''
-            Spacing between elements.
-          '';
-        };
-        orientation = {
-          type = types.nullOr types.str;
-          default = null;
-          description = ''
-            Orientation of the box. possible values: "vertical", "v", "horizontal", "h".
-          '';
-        };
-        space-evenly = {
-          type = types.bool;
-          default = false;
-          description = ''
-            Space the widgets evenly.
-          '';
-        };
-        icon-size = {
-          type = types.nullOr types.positive;
-          default = null;
-          description = ''
-            Size of icons in the tray.
-          '';
-        };
-        prepend-new = {
-          type = types.bool;
-          default = true;
-          description = ''
-            Prepend new icons.
-          '';
-        };
-      } // widgetDefaultAttributes;
-    }
-  );
 in
 {
   options.eww = {
@@ -581,7 +223,11 @@ in
     };
 
     defWidget = mkOption {
-      type = with types; attrsOf defWidgetOptions;
+      type = types.str;
+    };
+
+    scssPath = mkOption {
+      type = types.path;
     };
   };
 
@@ -593,7 +239,7 @@ in
           ''
           (defwindow ${name}
             :monitor ${toString defWindow.monitor}
-              :geometry (geometry
+            :geometry (geometry
                 :x "${defWindow.geometry.x}"
                 :y "${defWindow.geometry.y}"
                 :width "${defWindow.geometry.width}"
@@ -620,20 +266,22 @@ in
           config.eww.defListen}
         ${lib.concatMapAttrsStringSep "\n" (name: value:
           ''
-          (defvar ${name}: ${value}")
+          (defvar ${name} ${value})
           ''
           )
           config.eww.defVar}
         ${lib.concatMapAttrsStringSep "\n" (name: defPoll:
           ''
           (defpoll ${name}
-            :interval: "${defPoll.interval}"
+            :interval "${defPoll.interval}"
             `${defPoll.command}`
           )
           ''
           )
           config.eww.defPoll}
+        ${config.eww.defWidget}
       '';
+      "eww/eww.scss".source = config.eww.scssPath;
     };
   };
 }
