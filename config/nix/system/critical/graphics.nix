c2b1da9ec4baebc6be8988fba4acd717c8e87b7d
Nissyaniss <nissya@proton.me>
2026-05-10 16:10:59 +0200

    automount for pc

--- config/nix/system/critical/graphics.nix
+++ config/nix/system/critical/graphics.nix
@@ -34,6 +34,7 @@ in
 
   hardware.graphics = {
     enable = true;
+    enable32Bit = true;
   };
 
   hardware.nvidia = {
--- configuration.nix
+++ configuration.nix
@@ -1,4 +1,6 @@
-{ ... }:
+{ machine-name
+, ...
+}:
 {
   nixpkgs.config.permittedInsecurePackages = [
     "electron-38.8.4"
@@ -9,13 +11,26 @@
     ./config/nix
   ];
 
-  # nixpkgs.overlays = [
-  #   # Skipping tests while upstream sorts it out, revert once
-  #   # Hydra consistently builds openldap green.
-  #   (final: prev: {
-  #     openldap = prev.openldap.overrideAttrs (_: {
-  #       doCheck = false;
-  #     });
-  #   })
-  # ];
+  nixpkgs.overlays = [
+    # Skipping tests while upstream sorts it out, revert once
+    # Hydra consistently builds openldap green.
+    (final: prev: {
+      openldap = prev.openldap.overrideAttrs (_: {
+        doCheck = false;
+      });
+    })
+  ];
+
+  fileSystems."/mnt/data" = {
+    device = "/dev/disk/by-uuid/4979a195-a58c-4e11-ade3-c72d9b068986";
+    fsType = "ext4";
+    options = [
+      # If you don't have this options attribute, it'll default to "defaults" 
+      # boot options for fstab. Search up fstab mount options you can use
+      "x-gvfs-show"
+      "users" # Allows any user to mount and unmount
+      "nofail" # Prevent system from failing if this drive doesn't mount
+      "exec" # Permit execution of binaries and other executable files
+    ];
+  };
 }
