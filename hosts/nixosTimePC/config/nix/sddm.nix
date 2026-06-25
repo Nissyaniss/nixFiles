3ed944cfdbc4983a1791e6c72749a106ddbd2863
Nissyaniss <nissya@proton.me>
2026-06-25 17:48:47 +0200

    why did i added so much spaces

--- hosts/nixosTimePC/config/nix/sddm.nix
+++ hosts/nixosTimePC/config/nix/sddm.nix
@@ -10,16 +10,12 @@
     setupScript = [
       {
         monitor = "DP-1";
-
         resolution = "2560x1440";
-
         refreshRate = 144;
-
         primary = true;
       }
       {
         monitor = "HDMI-1";
-
         deactivate = true;
       }
     ];
