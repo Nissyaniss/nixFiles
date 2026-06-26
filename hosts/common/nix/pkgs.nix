1c11e04da89c4c2f3df9e052d70b23e0e7f89a3f
Nissyaniss <nissya@proton.me>
2026-06-26 16:40:50 +0200

    fixed sublime4 + update

--- hosts/common/nix/pkgs.nix
+++ hosts/common/nix/pkgs.nix
@@ -4,44 +4,52 @@
     "electron-38.8.4" # legcord
   ];
 
-  environment.systemPackages = with pkgs; [
-    bruno
-    fastfetch
-    neovim
-    feishin
-    wezterm
-    pipewire
-    sublime4
-    legcord
-    grimblast
-    nh
-    git
-    (python3.withPackages (ps: [ ps.psutil ]))
-    killall
-    networkmanagerapplet
-    playerctl
-    alsa-utils
-    sublime-merge-dev
-    nodejs
-    sddm-astronaut
-    nautilus
-    libnotify
-    vlc
-    eog
-    zathura
-    qt6.qt5compat
-    kdePackages.qtdeclarative
-    grim
-    hyprland
-    openssl
-    delta
-    upower
-    piper
-    # swaylock # for debugging the lock
-    jq
-    claude-code
-    zip
-    unzip
-    pear-desktop
-  ];
+  nixpkgs.config =
+    {
+      problems.handlers = {
+        sublimetext4.broken = "warn";
+      };
+    };
+
+  environment.systemPackages = with pkgs;
+    [
+      bruno
+      fastfetch
+      neovim
+      feishin
+      wezterm
+      pipewire
+      sublime4
+      legcord
+      grimblast
+      nh
+      git
+      (python3.withPackages (ps: [ ps.psutil ]))
+      killall
+      networkmanagerapplet
+      playerctl
+      alsa-utils
+      sublime-merge-dev
+      nodejs
+      sddm-astronaut
+      nautilus
+      libnotify
+      vlc
+      eog
+      zathura
+      qt6.qt5compat
+      kdePackages.qtdeclarative
+      grim
+      hyprland
+      openssl
+      delta
+      upower
+      piper
+      # swaylock # for debugging the lock
+      jq
+      claude-code
+      zip
+      unzip
+      pear-desktop
+    ];
 }
